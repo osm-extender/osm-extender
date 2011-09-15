a0e88b3b4891b82d7f35cc654604493fa3ac5409
Robert Gauld <robert@robertgauld.co.uk>
2011-09-15 15:00:38 +0100

    Make root page dynamic.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SectionManagementSystem::Application.routes.draw do
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +51,7 @@ SectionManagementSystem::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
