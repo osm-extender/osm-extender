9aab4778b2b0888f37521c5dbcac89c3fa8a8324
Robert Gauld <robert@robertgauld.co.uk>
2011-10-08 10:32:58 +0100

    Fix syntax error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def activation_needed(user)
     @user = user
-    @url  = build_url("/activate_account/#{user.activation_token}"
+    @url  = build_url("/activate_account/#{user.activation_token}")
     mail ({
       :subject => build_subject('Activate Your Account'),
       :to => build_email_address
@@ -20,7 +20,7 @@ class UserMailer < ActionMailer::Base
 
   def reset_password(user)
     @user = user
-    @url  = build_url("/reset_password/#{user.reset_password_token}"
+    @url  = build_url("/reset_password/#{user.reset_password_token}")
     mail ({
       :subject => build_subject('Password Reset'),
       :to => build_email_address
