1f399c28da0900f9fb1e06cfe62cca89fd820546
Robert Gauld <robert@robertgauld.co.uk>
2011-10-08 09:59:08 +0100

    Stop error if machine.yml does not exist

--- config/initializers/setup_mailers.rb
+++ config/initializers/setup_mailers.rb
@@ -1,16 +1,18 @@
 require File.join(Rails.root, 'lib', 'development_mail_interceptor')
 
-machine_configuration = YAML.load(IO.read(File.join(Rails.root, 'config', 'machine.yml')))
-
-ActionMailer::Base.smtp_settings = {
-  :address              => machine_configuration['mailer_server'],
-  :port                 => machine_configuration['mailer_port'] || 25,
-  :domain               => machine_configuration['mailer_domain'],
-  :user_name            => machine_configuration['mailer_username'],
-  :password             => machine_configuration['mailer_password'],
-  :authentication       => machine_configuration['mailer_authenticaion'] || 'plain',
-  :enable_starttls_auto => true
-}
+if File.exists?(File.join(Rails.root, 'config', 'machine.yml'))
+  machine_configuration = YAML.load(IO.read(File.join(Rails.root, 'config', 'machine.yml')))
+  
+  ActionMailer::Base.smtp_settings = {
+    :address              => machine_configuration['mailer_server'],
+    :port                 => machine_configuration['mailer_port'] || 25,
+    :domain               => machine_configuration['mailer_domain'],
+    :user_name            => machine_configuration['mailer_username'],
+    :password             => machine_configuration['mailer_password'],
+    :authentication       => machine_configuration['mailer_authenticaion'] || 'plain',
+    :enable_starttls_auto => true
+  }
+end
 
 ActionMailer::Base.default_url_options[:host] = 'localhost:3000'
 ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
