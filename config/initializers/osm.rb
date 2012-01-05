874da4c86aa4b859ca2fc0306d3e05effc2bb59a
Robert Gauld <robert@robertgauld.co.uk>
2012-01-05 17:33:37 +0000

    Fix: OSM::API looses API configuration set by initalizer in development load

--- config/initializers/osm.rb
+++ config/initializers/osm.rb
@@ -1,4 +1,6 @@
-OSM::API.configure(
-  :api_id     => ENV['osmx_osm_id'],
-  :api_token  => ENV['osmx_osm_token'],
-)
+ActionDispatch::Callbacks.to_prepare do
+  OSM::API.configure(
+    :api_id     => ENV['osmx_osm_id'],
+    :api_token  => ENV['osmx_osm_token'],
+  )
+end
