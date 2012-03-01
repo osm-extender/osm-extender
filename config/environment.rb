0e6fffa9802f1bd2a8d0340224f616813c4c9791
Robert Gauld <robert@robertgauld.co.uk>
2012-03-01 15:31:24 +0000

    Fix Root URL not being correctly set in emails. closes #33

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Load custom configuration
-require File.join(Rails.root, 'config', 'environments', "#{Rails.env}_custom.rb") if File.exists?(File.join(Rails.root, 'config', 'environments', "#{Rails.env}_custom.rb"))
-
 # Initialize the rails application
 OSMExtender::Application.initialize!
+
+# Load custom configuration
+require File.join(Rails.root, 'config', 'environments', "#{Rails.env}_custom.rb") if File.exists?(File.join(Rails.root, 'config', 'environments', "#{Rails.env}_custom.rb"))
