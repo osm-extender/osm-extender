2571785fc78dde1e157ad42e5a5cda4e98a1bbf3
Robert Gauld <robert@robertgauld.co.uk>
2012-03-01 11:03:56 +0000

    Fix "No such file or directory - db:seed" when seeding database on some machines.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 def prompt(question)
-  puts "\t#{question}?"
-  print "> "
-  gets.strip
+  STDOUT.puts "\t#{question}?"
+  STDOUT.print "> "
+  STDIN.gets.strip
 end
 
 # This file should contain all the record creation needed to seed the database with its default values.
