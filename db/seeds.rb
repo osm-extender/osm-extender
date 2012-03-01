adbcbc122e99d776b61fa7c70f9ea6083a73457b
Robert Gauld <robert@robertgauld.co.uk>
2012-03-01 11:00:29 +0000

    Remove double question mark in prompts when seeding the database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,49 +15,49 @@ unless Rails.env.test?
   SettingValue.create ([
     {
       :key => 'contact us - to',
-      :value => prompt('What email address should contact us form submissions be sent to?')
+      :value => prompt('What email address should contact us form submissions be sent to')
     }, {
       :key => 'notifier mailer - from',
-      :value => prompt('What email address should notifier mails be sent from?')
+      :value => prompt('What email address should notifier mails be sent from')
     }, {
       :key => 'user mailer - from',
-      :value => prompt('What email address should user related mails be sent from?')
+      :value => prompt('What email address should user related mails be sent from')
     }, {
       :key => 'reminder mailer - from',
-      :value => prompt('What email address should reminder mails be sent from?')
+      :value => prompt('What email address should reminder mails be sent from')
     }, {
       :key => 'reminder mailer - send failed to',
-      :value => prompt('What email address should reminder failure mails be sent to?')
+      :value => prompt('What email address should reminder failure mails be sent to')
     }, {
       :key => 'OSM API - id',
-      :value => prompt('What is the OSM API ID to use?')
+      :value => prompt('What is the OSM API ID to use')
     }, {
       :key => 'OSM API - token',
-      :value => prompt('What is the OSM API token to use?')
+      :value => prompt('What is the OSM API token to use')
     }, {
       :key => 'OSM API - name',
-      :value => prompt('What is the name displayed on OSM\'s External Access tab for this API?')
+      :value => prompt('What is the name displayed on OSM\'s External Access tab for this API')
     }, {
       :key => 'ReCAPTCHA - public key',
-      :value => prompt('What is the public key to use with ReCAPTCHA?')
+      :value => prompt('What is the public key to use with ReCAPTCHA')
     }, {
       :key => 'ReCAPTCHA - private key',
-      :value => prompt('What is the private key to use with ReCAPTCHA?')
+      :value => prompt('What is the private key to use with ReCAPTCHA')
     }, {
       :key => 'Mail Server - Address',
-      :value => prompt('What is the address of your SMTP server?')
+      :value => prompt('What is the address of your SMTP server')
     }, {
       :key => 'Mail Server - Port',
-      :value => prompt('What port on the SMTP server should be used?')
+      :value => prompt('What port on the SMTP server should be used')
     }, {
       :key => 'Mail Server - Domain',
-      :value => prompt('What is your login domain on the SMTP server?')
+      :value => prompt('What is your login domain on the SMTP server')
     }, {
       :key => 'Mail Server - Username',
-      :value => prompt('What is your username on the SMTP server?')
+      :value => prompt('What is your username on the SMTP server')
     }, {
       :key => 'Mail Server - Password',
-      :value => prompt('What is your password on the SMTP server?')
+      :value => prompt('What is your password on the SMTP server')
     }
   ])
 end
@@ -66,6 +66,6 @@ end
 if Rails.env.development?
   SettingValue.create({
     :key => 'Mail Server - Development recipient',
-    :value => prompt('Where should emails actually be sent to in the development environment?')  
+    :value => prompt('Where should emails actually be sent to in the development environment')  
   })
 end
