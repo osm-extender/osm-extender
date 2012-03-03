fafeb21466c076b45e08f1f3c9f940e9855f3f21
Robert Gauld <robert@robertgauld.co.uk>
2012-03-03 17:04:16 +0000

    Only load jqery scripts and CSS as well as google charts scripts when being used.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,4 @@
 // javascripts (or manifests) for specific controllers should be
 // called <controller>.js(.coffee), for example projects.js.
 //
-//=require jquery
-//=require jquery_ujs
 //= require_self
