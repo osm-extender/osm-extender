723af29d14d21b1e7cce3420ff4a4f1c64df8247
Robert Gauld <robert@robertgauld.co.uk>
2012-03-04 18:14:13 +0000

    Tidy user's javascript file.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,5 @@
 $(function() {
   $("#users th a, #users .pagination a").live("click", function() {
-//    $.get($("#users").attr("action"), $("#users").serialize(), function(data, textStatus, jqXHR){}, "script");
     $.getScript(this.href);
     return false;
   });
@@ -8,7 +7,6 @@ $(function() {
 
 function filter_table(input) {
   $.get($("#users").attr("action"), $("#users").serialize(), function(data, textStatus, jqXHR){
-//      $("#"+input.id)[0].focus;
   }, "script");
   return false;
 }
