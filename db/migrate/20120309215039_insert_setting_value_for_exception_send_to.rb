80fc8bc32858bbded5b78ec37741c94ec0ca469d
Robert Gauld <robert@robertgauld.co.uk>
2012-03-09 23:51:29 +0000

    Fix test data

--- db/migrate/20120309215039_insert_setting_value_for_exception_send_to.rb
+++ db/migrate/20120309215039_insert_setting_value_for_exception_send_to.rb
@@ -1,16 +1,20 @@
 class InsertSettingValueForExceptionSendTo < ActiveRecord::Migration
 
   def self.up
-    SettingValue.create ([
-      {
-        :key => 'notifier mailer - send exception to',
-        :value => self.prompt('What address should exception notifications be sent to')
-      },
-    ])
+    unless Rails.env.test?
+      SettingValue.create ([
+        {
+          :key => 'notifier mailer - send exception to',
+          :value => self.prompt('What address should exception notifications be sent to')
+        },
+      ])
+    end
   end
 
   def self.down
-    SettingValue.find_by_key('notifier mailer - send exception to').delete
+    unless Rails.env.test?
+      SettingValue.find_by_key('notifier mailer - send exception to').delete
+    end
   end
 
 
--- db/migrate/20120309224045_change_setting_value_key_for_reminder_failure_to.rb
+++ db/migrate/20120309224045_change_setting_value_key_for_reminder_failure_to.rb
@@ -1,15 +1,19 @@
 class ChangeSettingValueKeyForReminderFailureTo < ActiveRecord::Migration
 
   def self.up
-    sv = SettingValue.find_by_key!('reminder mailer - send failed to')
-    sv.key = 'notifier mailer - send failed reminder to'
-    sv.save!
+    unless Rails.env.test?
+      sv = SettingValue.find_by_key!('reminder mailer - send failed to')
+      sv.key = 'notifier mailer - send failed reminder to'
+      sv.save!
+    end
   end
 
   def self.down
-    sv = SettingValue.find_by_key!('notifier mailer - send failed reminder to')
-    sv.key = 'reminder mailer - send failed to'
-    sv.save!
+    unless Rails.env.test?
+      sv = SettingValue.find_by_key!('notifier mailer - send failed reminder to')
+      sv.key = 'reminder mailer - send failed to'
+      sv.save!
+    end
   end
 
 end
