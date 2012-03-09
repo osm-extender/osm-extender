d4da3cc55492951837974082e79b8fb783ebc6c6
Robert Gauld <robert@robertgauld.co.uk>
2012-03-09 15:33:08 +0000

    Setting values are now populated by migrations (like FAQs values will probably be added/rermoved as the code base evolves.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
-def prompt(question)
-  STDOUT.puts "#{question}?"
-  STDOUT.print "> "
-  STDIN.gets.strip
-end
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -11,64 +5,3 @@ end
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-unless Rails.env.test?
-  SettingValue.create ([
-    {
-      :key => 'contact us - to',
-      :value => prompt('What email address should contact us form submissions be sent to')
-    }, {
-      :key => 'notifier mailer - from',
-      :value => prompt('What email address should notifier mails be sent from')
-    }, {
-      :key => 'user mailer - from',
-      :value => prompt('What email address should user related mails be sent from')
-    }, {
-      :key => 'reminder mailer - from',
-      :value => prompt('What email address should reminder mails be sent from')
-    }, {
-      :key => 'reminder mailer - send failed to',
-      :value => prompt('What email address should reminder failure mails be sent to')
-    }, {
-      :key => 'OSM API - id',
-      :value => prompt('What is the OSM API ID to use')
-    }, {
-      :key => 'OSM API - token',
-      :value => prompt('What is the OSM API token to use')
-    }, {
-      :key => 'OSM API - name',
-      :value => prompt('What is the name displayed on OSM\'s External Access tab for this API')
-    }, {
-      :key => 'ReCAPTCHA - public key',
-      :value => prompt('What is the public key to use with ReCAPTCHA')
-    }, {
-      :key => 'ReCAPTCHA - private key',
-      :value => prompt('What is the private key to use with ReCAPTCHA')
-    }, {
-      :key => 'Mail Server - Address',
-      :value => prompt('What is the address of your SMTP server')
-    }, {
-      :key => 'Mail Server - Port',
-      :value => prompt('What port on the SMTP server should be used')
-    }, {
-      :key => 'Mail Server - Domain',
-      :value => prompt('What is your login domain on the SMTP server')
-    }, {
-      :key => 'Mail Server - Username',
-      :value => prompt('What is your username on the SMTP server')
-    }, {
-      :key => 'Mail Server - Password',
-      :value => prompt('What is your password on the SMTP server')
-    }, {
-      :key => 'signup code',
-      :value => prompt('What signup code would you like to require users to use (if blank then no code will be asked for)')
-    }
-  ])
-end
-
-
-if Rails.env.development?
-  SettingValue.create({
-    :key => 'Mail Server - Development recipient',
-    :value => prompt('Where should emails actually be sent to in the development environment')  
-  })
-end
