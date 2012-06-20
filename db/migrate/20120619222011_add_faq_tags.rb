b708422b70bcb80c3acb6d810587ac4a3b76d0fe
Robert Gauld <robert@robertgauld.co.uk>
2012-06-20 20:40:36 +0100

    Forgot to remove the adding of data from this migration.

--- db/migrate/20120619222011_add_faq_tags.rb
+++ db/migrate/20120619222011_add_faq_tags.rb
@@ -16,29 +16,6 @@ class AddFaqTags < ActiveRecord::Migration
     end
     add_index :faq_tagings, :faq_id
     add_index :faq_tagings, :faq_tag_id
-
-
-    FaqTag.reset_column_information
-    FaqTaging.reset_column_information
-    ft_osmx = FaqTag.create(:name => 'Online Scout Manager Extender')
-    ft_features = FaqTag.create(:name => 'Features')
-
-    say "Adding tags to FAQs"
-    {
-      "What's Online Scout Manager Extender" => [ft_osmx],
-      "Is it safe to give this site my Online Scout Manager password?" => [ft_osmx],
-      "I would like Online Scout Manager Extender to ..." => [ft_osmx],
-      "What's the OSM permissions page?" => [ft_features],
-      "What's the Email lists feature?" => [ft_features],
-      "What's the Programme review feature?" => [ft_features],
-      "What's the Email reminder feature?" => [ft_features],
-      "What's the Programme wizard feature?" => [ft_features],
-    }.each do |key, value|
-      say key, true
-      faq = Faq.find_by_question(key)
-      faq.tags = value
-      faq.save
-    end
   end
 
 
