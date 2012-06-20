107b120b757580f5e94235119fef459b35f1f25c
Robert Gauld <robert@robertgauld.co.uk>
2012-06-20 17:08:54 +0100

    Fix migrations.

--- db/migrate/20120410122949_add_description_to_setting_value.rb
+++ db/migrate/20120410122949_add_description_to_setting_value.rb
@@ -1,7 +1,7 @@
 class AddDescriptionToSettingValue < ActiveRecord::Migration
 
   def self.up
-    add_column :setting_values, :description, :text, :null=>false
+    add_column :setting_values, :description, :text, :null=>false, :default=>'Ooops, a description of this setting should appear here'
     change_column :setting_values, :key, :string, :null=>false
   end
 
