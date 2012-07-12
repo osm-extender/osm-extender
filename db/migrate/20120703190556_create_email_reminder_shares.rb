381601648705a029a23af25de69c48533aa123b3
Robert Gauld <robert@robertgauld.co.uk>
2012-07-12 15:02:01 +0100

    Make EmailReminderShare's auth_code column large enough to hold the code.

--- db/migrate/20120703190556_create_email_reminder_shares.rb
+++ db/migrate/20120703190556_create_email_reminder_shares.rb
@@ -5,7 +5,7 @@ class CreateEmailReminderShares < ActiveRecord::Migration
       t.string :email_address, :null => false
       t.string :name, :null => false
       t.string :state, :null => false, :limit => 16, :default => :pending
-      t.string :auth_code, :null => false, :limit => 64
+      t.string :auth_code, :null => false, :limit => 128
 
       t.timestamps
     end
