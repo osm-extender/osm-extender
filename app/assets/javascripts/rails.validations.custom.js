b4dc2177e1414b8e2f95579bbbeb80c2bdb4bddc
Robert Gauld <robert@robertgauld.co.uk>
2012-11-14 09:02:26 +0000

    Fix regex in date validators

--- app/assets/javascripts/rails.validations.custom.js
+++ app/assets/javascripts/rails.validations.custom.js
@@ -11,7 +11,7 @@ clientSideValidations.validators.local["time24h_format"] = function(element, opt
 }
 
 clientSideValidations.validators.local["date_format"] = function(element, options) {
-  if (!/^[0-9]{4}-(?:0[1-9]|1[0-2])-(?:[0-2][1-9]|3[0-1])$/i.test(element.val())) {
+  if (!/^[0-9]{4}-(?:0[1-9]|1[0-2])-(?:0[1-9]|[12][0-9]|3[0-1])$/i.test(element.val())) {
     return options.message;
   }
 }
