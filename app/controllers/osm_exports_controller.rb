09065042d3fb552338ee7bd6c8bce0d3fb5ff884
Robert Gauld <robert@robertgauld.co.uk>
2013-04-10 22:15:22 +0100

    Make exports controller DRYer

--- app/controllers/osm_exports_controller.rb
+++ app/controllers/osm_exports_controller.rb
@@ -15,6 +15,7 @@ class OsmExportsController < ApplicationController
     @flexi_records = Hash[current_section.flexi_records.sort.map{ |f| [f.name, f.id] } ]
   end
 
+
   def flexi_record
     require_osm_permission :read, :flexi
     flexi_record = nil
@@ -36,39 +37,122 @@ class OsmExportsController < ApplicationController
         system_fields.push field
       end
     end
+
     fields = [*system_fields, *custom_fields]
+    headers = ['Member ID', *fields.map{ |f| f.name }]
     records = flexi_record.get_data(current_user.osm_api).map{ |r| [
       r.member_id,
       *r.fields.values_at(*fields.map{ |f| f.id })
     ]}
 
-    csv_options = {
-      :col_sep => get_column_separator,
-      :write_headers => params[:file_options][:include_header],
-      :force_quotes => params[:file_options][:force_quotes],
-      :quote_char => params[:file_options][:quote] || '"',
-      :skip_blanks => true,
-      :headers => ['Member ID', *fields.map{ |f| f.name }]
-    }
-    send_csv(flexi_record.name, csv_options, records)
+    send_csv(flexi_record.name, headers, records)
   end
+
 
   def members
     require_osm_permission :read, :member
-    data = get_members_data
-    csv_options = {
-      :col_sep => get_column_separator,
-      :write_headers => params[:file_options][:include_header],
-      :force_quotes => params[:file_options][:force_quotes],
-      :quote_char => params[:file_options][:quote] || '"',
-      :skip_blanks => true,
-      :headers => data[:headers]
+
+    members = Osm::Member.get_for_section(current_user.osm_api, current_section, params[:term_id])
+    groupings = get_current_section_groupings.invert
+
+    headers = [
+      'ID',
+      'First Name',
+      'Last Name',
+      'Grouping',
+      'Age',
+      current_section.column_names[:phone1],
+      current_section.column_names[:phone2],
+      current_section.column_names[:phone3],
+      current_section.column_names[:phone4],
+      current_section.column_names[:email1],
+      current_section.column_names[:email2],
+      current_section.column_names[:email3],
+      current_section.column_names[:email4],
+      current_section.column_names[:address],
+      current_section.column_names[:address2],
+      current_section.column_names[:parents],
+      current_section.column_names[:notes],
+      current_section.column_names[:medical],
+      current_section.column_names[:subs],
+      current_section.column_names[:religion],
+      current_section.column_names[:school],
+      current_section.column_names[:ethnicity],
+      current_section.column_names[:custom1],
+      current_section.column_names[:custom2],
+      current_section.column_names[:custom3],
+      current_section.column_names[:custom4],
+      current_section.column_names[:custom5],
+      current_section.column_names[:custom6],
+      current_section.column_names[:custom7],
+      current_section.column_names[:custom8],
+      current_section.column_names[:custom9],
+      'Date of Birth',
+      'Joined Scouting',
+      'Started Section',
+      'Section ID',
+      'Grouping ID',
+      'Grouping Leader',
+    ]
+
+    members.map! { |i|
+      member = i.attributes.values_at(
+        'id',
+        'first_name',
+        'last_name',
+        '', # Will be filled with grouping name later
+        'age',
+        'phone1',
+        'phone2',
+        'phone3',
+        'phone4',
+        'email1',
+        'email2',
+        'email3',
+        'email4',
+        'address',
+        'address2',
+        'parents',
+        'notes',
+        'medical',
+        'subs',
+        'religion',
+        'school',
+        'ethnicity',
+        'custom1',
+        'custom2',
+        'custom3',
+        'custom4',
+        'custom5',
+        'custom6',
+        'custom7',
+        'custom8',
+        'custom9',
+        'date_of_birth',
+        'joined',
+        'started',
+        'section_id',
+        'grouping_id',
+        'grouping_leader'
+      )
+      member[3] = groupings[i.grouping_id] # Grouping name
+      member
     }
-    send_csv('Members', csv_options, data[:items])
+
+    send_csv('Members', headers, members)
   end
+
 
   def programme_activities
     require_osm_permission :read, :programme
+
+    headers = [
+      'Meeting ID',
+      'Activity ID',
+      'Title',
+      'Notes',
+    ]
+
     data = []
     Osm::Meeting.get_for_section(current_user.osm_api, current_section, params[:term_id]).each do |meeting|
       meeting.activities.each do |activity|
@@ -76,24 +160,25 @@ class OsmExportsController < ApplicationController
       end
     end
 
-    csv_options = {
-      :col_sep => get_column_separator,
-      :write_headers => params[:file_options][:include_header],
-      :force_quotes => params[:file_options][:force_quotes],
-      :quote_char => params[:file_options][:quote] || '"',
-      :skip_blanks => true,
-      :headers => [
-        'Meeting ID',
-        'Activity ID',
-        'Title',
-        'Notes',
-      ]
-    }
-    send_csv('Programme Activities', csv_options, data)
+    send_csv('Programme Activities', headers, data)
   end
 
   def programme_meetings
     require_osm_permission :read, :programme
+
+    headers = [
+      'ID',
+      'Date',
+      'Start Time',
+      'Finish Time',
+      'Title',
+      'Notes for Parents',
+      'Pre Notes',
+      'Post Notes',
+      'Leaders',
+      'Games',
+    ]
+
     data = Osm::Meeting.get_for_section(current_user.osm_api, current_section, params[:term_id]).map { |i|
       i.attributes.values_at(
         'id',
@@ -109,45 +194,21 @@ class OsmExportsController < ApplicationController
       )
     }
 
-    csv_options = {
+    send_csv('Programme Meetings', headers, data)
+  end
+
+
+  private
+  def send_csv(data_label, headers, data)
+    options = {
       :col_sep => get_column_separator,
       :write_headers => params[:file_options][:include_header],
       :force_quotes => params[:file_options][:force_quotes],
       :quote_char => params[:file_options][:quote] || '"',
       :skip_blanks => true,
-      :headers => [
-        'ID',
-        'Date',
-        'Start Time',
-        'Finish Time',
-        'Title',
-        'Notes for Parents',
-        'Pre Notes',
-        'Post Notes',
-        'Leaders',
-        'Games',
-      ]
+      :headers => headers,
     }
-    send_csv('Programme Meetings', csv_options, data)
-  end
 
-
-  private
-  def get_filename_extension
-    filename_extension = {
-      'TAB' => 'tsv',
-      ',' => 'csv',
-      'PIPE' => 'psv'
-    }[params[:file_options][:column_separator]]
-    return filename_extension || "#{params[:file_options][:column_separator][0]}sv"
-  end
-
-  def get_column_separator
-    column_separator = {'TAB' => "\t", 'PIPE' => '|'}[params[:file_options][:column_separator]]
-    return column_separator || params[:file_options][:column_separator][0]
-  end
-
-  def send_csv(data_label, options, data)
     extension = get_filename_extension
     filename = [
       current_section.group_name.gsub(' ', '_').camelize,
@@ -163,94 +224,18 @@ class OsmExportsController < ApplicationController
     send_data csv_string, :filename => filename, :type => "text/#{extension}", :disposition => 'attachment'
   end
 
+  def get_filename_extension
+    filename_extension = {
+      'TAB' => 'tsv',
+      ',' => 'csv',
+      'PIPE' => 'psv'
+    }[params[:file_options][:column_separator]]
+    return filename_extension || "#{params[:file_options][:column_separator][0]}sv"
+  end
 
-  def get_members_data
-    items = Osm::Member.get_for_section(current_user.osm_api, current_section, params[:term_id])
-    groupings = get_current_section_groupings.invert
-    return {
-      :headers => [
-        'ID',
-        'First Name',
-        'Last Name',
-        'Grouping',
-        'Age',
-        current_section.column_names[:phone1],
-        current_section.column_names[:phone2],
-        current_section.column_names[:phone3],
-        current_section.column_names[:phone4],
-        current_section.column_names[:email1],
-        current_section.column_names[:email2],
-        current_section.column_names[:email3],
-        current_section.column_names[:email4],
-        current_section.column_names[:address],
-        current_section.column_names[:address2],
-        current_section.column_names[:parents],
-        current_section.column_names[:notes],
-        current_section.column_names[:medical],
-        current_section.column_names[:subs],
-        current_section.column_names[:religion],
-        current_section.column_names[:school],
-        current_section.column_names[:ethnicity],
-        current_section.column_names[:custom1],
-        current_section.column_names[:custom2],
-        current_section.column_names[:custom3],
-        current_section.column_names[:custom4],
-        current_section.column_names[:custom5],
-        current_section.column_names[:custom6],
-        current_section.column_names[:custom7],
-        current_section.column_names[:custom8],
-        current_section.column_names[:custom9],
-        'Date of Birth',
-        'Joined Scouting',
-        'Started Section',
-        'Section ID',
-        'Grouping ID',
-        'Grouping Leader',
-      ],
-      :items => items.map { |i|
-        data = i.attributes.values_at(
-          'id',
-          'first_name',
-          'last_name',
-          '', # Will be filled with grouping name later
-          'age',
-          'phone1',
-          'phone2',
-          'phone3',
-          'phone4',
-          'email1',
-          'email2',
-          'email3',
-          'email4',
-          'address',
-          'address2',
-          'parents',
-          'notes',
-          'medical',
-          'subs',
-          'religion',
-          'school',
-          'ethnicity',
-          'custom1',
-          'custom2',
-          'custom3',
-          'custom4',
-          'custom5',
-          'custom6',
-          'custom7',
-          'custom8',
-          'custom9',
-          'date_of_birth',
-          'joined',
-          'started',
-          'section_id',
-          'grouping_id',
-          'grouping_leader'
-        )
-        data[3] = groupings[i.grouping_id] # Grouping name
-        data
-      }
-    }
+  def get_column_separator
+    column_separator = {'TAB' => "\t", 'PIPE' => '|'}[params[:file_options][:column_separator]]
+    return column_separator || params[:file_options][:column_separator][0]
   end
 
 end
