5df954dae01eb6462be5680fda657e0dd5ee0608
Robert Gauld <robert@robertgauld.co.uk>
2013-04-16 17:02:48 +0100

    Fix creating of cached terms for balanced programme review.

--- app/controllers/programme_review_balanced_cache_controller.rb
+++ app/controllers/programme_review_balanced_cache_controller.rb
@@ -2,12 +2,12 @@ class ProgrammeReviewBalancedCacheController < ApplicationController
   load_and_authorize_resource
 
   def destroy
-    ProgrammeReviewBalancedCache.for_section(current_section).find(params[:id]).delete
+    ProgrammeReviewBalancedCache.for_section(current_section).find(params[:id]).destroy
     redirect_to programme_review_balanced_path
   end
 
   def destroy_multiple
-    ProgrammeReviewBalancedCache.for_section(current_section).delete_all(:term_id => params[:term_ids])
+    ProgrammeReviewBalancedCache.for_section(current_section).destroy_all(:id => params[:ids])
     redirect_to programme_review_balanced_path
   end
 
--- app/models/programme_review_balanced_cache.rb
+++ app/models/programme_review_balanced_cache.rb
@@ -11,7 +11,7 @@ class ProgrammeReviewBalancedCache < ActiveRecord::Base
   validates_presence_of :data
   validates_presence_of :last_used_at
 
-  scope :for_section, ->(section) { where("section_id = ?", (section.is_a?(Fixnum) ? section : section.id)) }
+  scope :for_section, ->(section) { where("section_id == ?", section.to_i) }
 
   def self.delete_old(older_than=1.year.ago)
     if older_than.is_a?(String)
