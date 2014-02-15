ccdcde66bfe509226cf25ab9852636caa9d9b1ea
Robert Gauld <robert@robertgauld.co.uk>
2014-02-15 11:27:47 +0000

    Tidy contact_us model. addresses #134

--- app/models/contact_us.rb
+++ app/models/contact_us.rb
@@ -1,8 +1,5 @@
 class ContactUs
-
-  include ActiveModel::Validations
-  include ActiveModel::Conversion
-  extend ActiveModel::Naming
+  include ActiveModel::Model
 
   attr_accessor :name, :email_address, :message
 
@@ -13,11 +10,6 @@ class ContactUs
 
   validates_presence_of :message
 
-  def initialize(attributes = {})
-    attributes.each do |name, value|
-      send("#{name}=", value)
-    end
-  end
 
   def send_contact
     if valid?
@@ -25,10 +17,6 @@ class ContactUs
     else
       return nil
     end
-  end
-
-  def persisted?
-    false
   end
 
 end
