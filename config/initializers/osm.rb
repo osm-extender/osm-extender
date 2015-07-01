dca93be9c50976bf90c6778fb1714554b966234b
Robert Gauld <robert@robertgauld.co.uk>
2015-07-01 20:04:04 +0100

    Prepare for merge into staging

--- config/initializers/osm.rb
+++ config/initializers/osm.rb
@@ -2,8 +2,8 @@ ActionDispatch::Callbacks.to_prepare do
   unless Rails.env.test?
     Osm::configure(
       :api => {
-        :default_site => :migration,
-        :migration => {
+        :default_site => :osm,
+        :osm => {
           :id    => Rails.application.secrets.osm_api[:id],
           :token => Rails.application.secrets.osm_api[:token],
           :name  => Rails.application.secrets.osm_api[:name],
