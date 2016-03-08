6756e41d72aecbb7612d9c99c01985aa2c209ac1
Robert Gauld <robert@robertgauld.co.uk>
2016-03-08 13:01:55 +0000

    Clean out unused tests

--- test/unit/email_reminder_item_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class EmailReminderItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/setting_value_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SettingValueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
