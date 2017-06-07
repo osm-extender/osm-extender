3071f2c4e14ed80c53bb3c1abec5ef9558193eff
Robert Gauld <robert@robertgauld.co.uk>
2017-06-07 10:48:43 +0100

    Send custom no term email from automation task errors. close #171

--- app/mailers/automation_task_mailer.rb
+++ app/mailers/automation_task_mailer.rb
@@ -19,4 +19,25 @@ class AutomationTaskMailer < ApplicationMailer
     })
   end
 
+  def no_current_term(task, exception)
+    @task = task
+    user = task.user
+
+    unless user.nil? || !user.connected_to_osm? || task.section_id.nil?
+      api = user.osm_api
+      @next_term = nil
+      @last_term = nil
+      terms = Osm::Term.get_for_section(api, task.section_id)
+      terms.each do |term|
+        @last_term = term if term.past? && (@last_term.nil? || term.finish > @last_term.finish)
+        @next_term = term if term.future? && (@next_term.nil? || term.start < @next_term.start)
+      end
+    end
+
+    mail ({
+      :subject => build_subject('Preparing Email Reminder FAILED'),
+      :to => task.user.email_address_with_name
+    })
+  end
+
 end
