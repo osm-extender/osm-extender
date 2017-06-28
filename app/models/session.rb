ae9ec7f64f557b63accf10ea26b5aa2498f90d9e
Robert Gauld <robert@robertgauld.co.uk>
2017-06-28 15:12:25 +0100

    CLeaning sessions now takes no arguments and uses the settings from sorcery config to determine when sessions are expired.

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,15 +5,8 @@ class Session < ActiveRecord::Base
   scope :guests, -> { where(user_id: nil) }
   scope :users, -> { where.not(user_id: nil) }
 
-  def self.delete_old_sessions(inactive_for=1.hour, total_age=6.hours)
-    if inactive_for.is_a?(String)
-      inactive_for = inactive_for.split.inject { |count, unit| count.to_i.send(unit) }
-    end
-    if total_age.is_a?(String)
-      total_age = total_age.split.inject { |count, unit| count.to_i.send(unit) }
-    end
-
-    destroy_all ['updated_at < ? OR created_at < ?', inactive_for.ago.to_s(:db), total_age.ago.to_s(:db)]
+  def self.delete_old_sessions
+    destroy_all ['updated_at < ?', Rails.application.config.sorcery.session_timeout.seconds.ago.to_s(:db)]
   end
 
 end
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,25 +1,10 @@
 describe Session do
 
-  describe '#delete_old_sessions' do
-
-    it 'Given nothing' do
-      Timecop.freeze(DateTime.new(2016, 10, 7, 23, 00))
-      expect(Session).to receive(:destroy_all).with(['updated_at < ? OR created_at < ?', '2016-10-07 22:00:00', '2016-10-07 17:00:00']).and_return(true)
-      described_class.delete_old_sessions()
-    end
-
-    it 'Given numbers' do
-      Timecop.freeze(DateTime.new(2016, 10, 7, 23, 00))
-      expect(Session).to receive(:destroy_all).with(['updated_at < ? OR created_at < ?', '2016-10-07 21:00:00', '2016-10-06 23:00:00']).and_return(true)
-      described_class.delete_old_sessions(2.hours, 1.day)
-    end
-
-    it 'Given strings' do
-      Timecop.freeze(DateTime.new(2016, 10, 7, 23, 00))
-      expect(Session).to receive(:destroy_all).with(['updated_at < ? OR created_at < ?', '2016-10-07 20:00:00', '2016-09-30 23:00:00']).and_return(true)
-      described_class.delete_old_sessions('3 hours', '1 week')
-    end
-
+  it '#delete_old_sessions' do
+    Timecop.freeze(DateTime.new(2016, 10, 7, 23, 00))
+    expect(Rails).to receive_message_chain(:application, :config, :sorcery, :session_timeout){ 60.minutes }
+    expect(Session).to receive(:destroy_all).with(['updated_at < ?', '2016-10-07 22:00:00']).and_return(true)
+    described_class.delete_old_sessions()
   end
 
 end
