b2b3c4b20f77aa8b9f02575d68448cb4f429e042
Robert Gauld <robert@robertgauld.co.uk>
2018-03-29 11:33:43 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Do rails stuff
+bundle install
+rake db:migrate
+rake assets:precompile
+
+# Reload unicorn
+if [ -e tmp/pids/unicorn.pid ]; then
+	kill -s USR2 `cat tmp/pids/unicorn.pid`
+fi
+
+# Notify rollbar
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$ROLLBAR_TOKEN_POST_SERVER_ITEM \
+  -F environment=$RAILES_ENV \
+  -F revision=`git rev-parse --verify HEAD` \
+  -F local_username=`whoami` \
+  -F rollbar_username=$ROLLBAR_USERNAME
