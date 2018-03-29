f7b2b7390492855cabf3fc1902abdd28b324df3f
Robert Gauld <robert@robertgauld.co.uk>
2018-03-29 15:59:11 +0100

    Fix rollbar part of deply script

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ fi
 # Notify rollbar
 curl https://api.rollbar.com/api/1/deploy/ \
   -F access_token=$ROLLBAR_TOKEN_POST_SERVER_ITEM \
-  -F environment=$RAILES_ENV \
+  -F environment=$RAILS_ENV \
   -F revision=`git rev-parse --verify HEAD` \
   -F local_username=`whoami` \
   -F rollbar_username=$ROLLBAR_USERNAME
