4cd3491eb1d13bff7d9f72d0a1a54c753ac4ebac
Robert Gauld <robert@robertgauld.co.uk>
2018-04-07 17:32:04 +0100

    Move travis to using multiple scripts not a rake task.

--- db/migrate/20170612173332_add_user_id_to_sessions.rb
+++ db/migrate/20170612173332_add_user_id_to_sessions.rb
@@ -2,7 +2,6 @@ class AddUserIdToSessions < ActiveRecord::Migration
   def change
     change_table :sessions do |t|
       t.references :user, index: true
-      t.index :session_id
     end
   end
 end
