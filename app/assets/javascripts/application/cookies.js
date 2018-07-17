0fc75a94aa7c210eec8c23b3ea0975a47ee5739b
Robert Gauld <robert@robertgauld.co.uk>
2018-07-17 14:53:56 +0100

    Allow assets to be compiled during docker image building

--- app/assets/javascripts/application/cookies.js
+++ app/assets/javascripts/application/cookies.js
@@ -2,7 +2,6 @@
 
 var cookiesAllowed = null;
 
-
 function allowCookies() {
   Cookies.set('allow_cookies', 'yes', {expires: 365});
   cookiesAllowed = 'yes';
@@ -11,15 +10,14 @@ function allowCookies() {
 }
 
 function cookiesPermitted() {
-<% if Figaro.env.ga_tracking_id? %>
-  var ga = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" +
-    "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
-    "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
-    "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" +
-    "ga('create', '<%= Figaro.env.ga_tracking_id %>', 'auto');" +
-    "ga('send', 'pageview');";
-  $('head').append('<script>' + ga + '</script>');
-<% end %>  
+  if ($('body').data('ga-tracking-id')) {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', $('body').data('ga-tracking-id'), 'auto');
+    ga('send', 'pageview');
+  }
 }
 
 function ready(){
