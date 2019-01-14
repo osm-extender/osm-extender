4a3f2a751d6b8005d6b8c7e8aa4dcdf8557fb1c8
Robert Gauld <robert@robertgauld.co.uk>
2019-01-14 10:01:58 +0000

    Split cron and non-cron jobs on administer delayed job page.

--- app/controllers/delayed_job_controller.rb
+++ app/controllers/delayed_job_controller.rb
@@ -4,7 +4,8 @@ class DelayedJobController < ApplicationController
   def index
     wanted_settings = [:default_priority, :max_attempts, :max_run_time, :sleep_delay, :destroy_failed_jobs, :delay_jobs]
     @settings = Hash[ wanted_settings.map{ |i| [i, Delayed::Worker.send(i)] } ]
-    @jobs = Delayed::Job.all
+    @crons = Delayed::Job.where.not(cron: nil)
+    @jobs = Delayed::Job.where(cron: nil)
   end
 
   def show
