f70688e89527817be7d66d07be328a6565388142
Robert Gauld <robert@robertgauld.co.uk>
2019-07-07 13:07:02 +0100

    Remove duplication of requiring env vars.

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,11 +1,1 @@
-Figaro.require_keys(
-  'secret_key_base',
-  'osm_api_name', 'osm_api_id', 'osm_api_token',
-  'recaptcha_public_key', 'recaptcha_private_key'
-)
-
-if Rails.configuration.action_mailer.delivery_method.eql?(:mailgun)
-  Figaro.require_keys(
-    'mailgun_domain', 'mailgun_api_key'
-  )
-end
+Figaro.require_keys 'secret_key_base'
